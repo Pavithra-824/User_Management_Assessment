b00040d83fc7a6d4ce052cc218b1ec0e88be9d24
Pavithra-824 <pavithrasasikalakannan@gmail.com>
2025-12-31 00:18:02 +0530

    pwd

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import AbstractUser
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 import re
 
 User = get_user_model()
@@ -27,11 +27,24 @@ class UserSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Password must contain at least one number.")
         return value
 
-    def create(self, validated_data) -> AbstractUser:
-        user = User.objects.create_user(
+    def create(self, validated_data):
+        return User.objects.create_user(
             email=validated_data['email'],
             username=validated_data['email'],
             full_name=validated_data['full_name'],
             password=validated_data['password']
         )
-        return user
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        # Apply the same strength rules to the new password
+        if len(value) < 8:
+            raise serializers.ValidationError("New password must be at least 8 characters.")
+        if not re.search(r'[A-Z]', value):
+            raise serializers.ValidationError("New password must have an uppercase letter.")
+        if not re.search(r'[0-9]', value):
+            raise serializers.ValidationError("New password must have a number.")
+        return value
